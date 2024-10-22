4a00c523542be28d38158c5267a6956bfbf1e71e
KIRALAINEisSTUPID <k9034449@gmail.com>
2024-10-22 19:32:22 +0500

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.views.generic import TemplateView
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+
+class BlogView(TemplateView):
+    template_name = 'blog.html'
