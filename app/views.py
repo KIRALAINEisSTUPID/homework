89fb286c7e3c922210d8a90d29545d344faf640d
KIRALAINEisSTUPID <k9034449@gmail.com>
2024-10-22 19:49:37 +0500

    updated

--- app/views.py
+++ app/views.py
@@ -1,9 +1,13 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+
+from app.models import Blogs
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
 
 
-class BlogView(TemplateView):
+class BlogView(ListView):
     template_name = 'blog.html'
+    model = Blogs
+    context_object_name = 'blogs'
